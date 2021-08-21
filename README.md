9ebee679a2c0f78c6fc3dfd8724ffc8ce9e43ed8
Matt Koski <mjkekai@gmail.com>
2021-08-20 20:15:18 -0700

    adds ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# -e-commerce
+# E-Commerce Back End
+
+![GitHub license](https://img.shields.io/badge/license-NONE-blue.svg)
+
+## Table of Contents (Optional)
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Description](#description)
+- [License](#license)
+- [Credits](#credits)
+- [Contributing](#contributing)
+
+## Description
+
+Back end of a online retail store
+
+## Usage
+
+Users are able to add, delete, and updates their products by product name, category, or ID
+
+## Contributing
+
+None
+
+## Installation
+
+npm run seed to seed the database. node index.js to start the server and connect to Insomnia
+
+## Credits
+
+none
+
+## License
+
+NONE
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,51 +1,57 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all categories
   // be sure to include its associated Products
-//   try {
-//     const categoryData = await Category.findAll();
-//     res.status(200).json(categoryData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
+  //   try {
+  //     const categoryData = await Category.findAll();
+  //     res.status(200).json(categoryData);
+  //   } catch (err) {
+  //     res.status(500).json(err);
+  //   }
 
-Category.findAll({ 
-  include: [
-    {
-      model: Product, as:'category_products'
-  },
-],
- })
- .then((categories) => res.status(200).json(categories)
- .catch((err) => {
-   res.status(500).json(err);
- }));
- 
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        as: "category_products",
+      },
+    ],
+  }).then((categories) =>
+    res
+      .status(200)
+      .json(categories)
+      .catch((err) => {
+        res.status(500).json(err);
+      })
+  );
 });
 
-router.get('/:id',  (req, res) => {
+router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
- Category.findByPk(req.params.id,{
-   include: [
-     {
-       model: Product, as: 'product_category'
-      }
-   ]
- })
- .then((findCategory) => res.status(200).json(findCategory)
- .catch((err) => {
-   console.log("categories get", err);
-   res.status(500).json(err);
- }));
-  
+  Category.findByPk(req.params.id, {
+    include: [
+      {
+        model: Product,
+        as: "product_category",
+      },
+    ],
+  }).then((findCategory) =>
+    res
+      .status(200)
+      .json(findCategory)
+      .catch((err) => {
+        console.log("categories get", err);
+        res.status(500).json(err);
+      })
+  );
 });
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
   try {
     const categoryData = await Category.create(req.body);
@@ -55,45 +61,41 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
   Category.update(
     {
-   
       id: req.body.id,
-      
     },
     {
-     
       where: {
         id: req.params.id,
       },
     }
   )
     .then((updatedCategory) => {
-    
       res.json(updatedCategory);
     })
     .catch((err) => res.json(err));
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
   try {
     const categoryData = await Category.destroy({
       where: {
-        id: req.params.id
-      }
-});
-if (!categoryData) {
-  res.status(404).json({ message: 'No Category found with this id!' });
-  return;
-}
+        id: req.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "No Category found with this id!" });
+      return;
+    }
 
-res.status(200).json(categoryData);
-} catch (err) {
-res.status(500).json(err);
-}
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
